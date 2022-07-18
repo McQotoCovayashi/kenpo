b33d60afce65bc1f986d98e9498119737b6289eb
McqOva <kyouikukoubou@gmail.com>
2022-07-18 19:52:12 +0900

    layout  and article update

--- home.py
+++ home.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import json
-import os
-import pprint
-
-# json_open = open("setting.json","r")
-# setting_json = json.load(json_open)
-# print(type(setting_json))
 
 with open('setting.json', 'r', encoding="UTF-8") as json_open:
     SETTING = json.load(json_open,)
@@ -19,14 +12,12 @@ st.header(SETTING['header'])
 st.subheader(SETTING['subheader'])
 st.caption(SETTING['caption'])
 
-
-name = st.selectbox("名称",SETTING['names'])
-select_options = ['前文'] + list(range(1,ARTICLE[name]['number']+1))
-# with st.form("入力先を選択",clear_on_submit=False):
-#     article_number = str(st.selectbox("第n条",select_options))
-#     st.form_submit_button("読込")
-
-article_number = str(st.selectbox("第n条",select_options))
+col0 , col1 = st.columns([2,1])
+with col0:
+    name = st.selectbox("名称",SETTING['names'])
+with col1:
+    select_options = ['前文'] + list(range(1,ARTICLE[name]['number']+1))
+    article_number = str(st.selectbox("第n条",select_options))
 
 if article_number in ARTICLE[name]:
     article_text = ARTICLE[name][article_number]['text']
@@ -38,16 +29,22 @@ else:
 article = {}
 # st.write(str(ARTICLE[name][article_number]['tags']))
 with st.form("条文の入力",clear_on_submit=True):
-    article['text'] = st.text_area("",value=article_text,placeholder="条文を書きます",height=250)
-    article['tags']  = st.multiselect("タグ",SETTING['tags'], default=default_tags)
-    submit_bool = st.form_submit_button("書込")
+    article['text'] = st.text_area("",value=article_text,placeholder="条文を書きます",height=500)
+    col2, col3 = st.columns([2,0.3])
+    with col2:
+        article['tags']  = st.multiselect("タグ",SETTING['tags'], default=default_tags)
+    with col3:
+        st.write("")
+        st.write("")
+        submit_bool = st.form_submit_button("書込")
     if submit_bool:
         ARTICLE[name][article_number] = article
         with open('articles.json','w', encoding="UTF-8") as article_open:
             json.dump(ARTICLE, article_open, indent=4,ensure_ascii=False)
         submit_bool = False
+        st.info('条文の書き込みを行いました')
 
-name=""
-article_number = 0
-article_text = ""
-default_tags = []
+    name=""
+    article_number = 0
+    article_text = ""
+    default_tags = []
